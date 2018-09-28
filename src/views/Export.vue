cb179b75415cc7525ae26a8722f9ad8684120707
Román Jiménez <roman@cartodb.com>
2018-09-28 11:48:04 +0200

    add export module

---
+++ src/views/Export.vue
@@ -0,0 +1,37 @@
+<template>
+
+<div>
+  <h1>Here's a preview of your map</h1>
+  <div>
+    <iframe class="iframe" src="https://carto.com"></iframe>
+  </div>
+  <h1>Export to</h1>
+  <button>Zip file</button>
+  <button>Codepen</button>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Export',
+  methods: {
+    exportZip() {
+      
+    },
+
+    exportCodePen() {
+
+    }
+  },
+}
+</script>
+
+
+<style>
+.iframe {
+  width: 1920px;
+  height: 1080px;
+  transform: scale(0.5) translate(-960px, -540px);
+}
+</style>
