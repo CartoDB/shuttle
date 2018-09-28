b531dbe5feca2f8adcc368f298a6b3181bd4565c
jesusbotella <jesus.botella@gmail.com>
2018-09-28 11:58:19 +0200

    Make default export option

--- src/generate-bundle/index.js
+++ src/generate-bundle/index.js
@@ -1,6 +1,5 @@
 import Scaffoldings from './scaffolding';
 import Partials from './partials';
-import { generateBundle } from './bundle';
 
 function convertToTemplate (baseData) {
   const options = {};
@@ -17,50 +16,51 @@ function convertToTemplate (baseData) {
   };
 }
 
-const baseData = {
-  ui: {
-    colors: {
-      primary: '#00FF00',
-      secondary: '#FABADA',
-      complementary: '#909090'
-    }
-  },
-  layout: {
-    toolbar: { position: 'left' },
-    sidebars: [
-      { position: 'left', size: 'l'},
-      { position: 'right', size: 'l'}
-    ],
-    panels: [
-      { horizontalPosition: 'top', verticalPosition: 'left'},
-      { horizontalPosition: 'top', verticalPosition: 'right'},
-      { horizontalPosition: 'bottom', verticalPosition: 'left'},
-      { horizontalPosition: 'bottom', verticalPosition: 'right'}
-    ],
-    footer: {}
-  },
-  visualization: {
-    library: 'cartovl',
-    data: {
-      apiKey: 'default_public',
-      username: 'roman-carto',
-      map: {
-        // bubbles, category, choropleth, gradient, flow
-        type: 'category',
-        geometry: 'polygon',
-        basemap: 'voyager',
-        extent: [-3.888962, 40.312064, -3.518051, 40.643271],
-        dataset: 'barrios',
-        column: 'nomdis',
-        // Only CARTO VL
-        columnType: 'string',
-        colorRamp: 'Bold'
-      }
-    }
-  }
-};
+// const baseData = {
+//   ui: {
+//     colors: {
+//       primary: '#00FF00',
+//       secondary: '#FABADA',
+//       complementary: '#909090'
+//     }
+//   },
+//   layout: {
+//     toolbar: { position: 'left' },
+//     sidebars: [
+//       { position: 'left', size: 'l'},
+//       { position: 'right', size: 'l'}
+//     ],
+//     panels: [
+//       { horizontalPosition: 'top', verticalPosition: 'left'},
+//       { horizontalPosition: 'top', verticalPosition: 'right'},
+//       { horizontalPosition: 'bottom', verticalPosition: 'left'},
+//       { horizontalPosition: 'bottom', verticalPosition: 'right'}
+//     ],
+//     footer: {}
+//   },
+//   visualization: {
+//     library: 'cartovl',
+//     data: {
+//       apiKey: 'default_public',
+//       username: 'roman-carto',
+//       map: {
+//         // bubbles, category, choropleth, gradient, flow
+//         type: 'category',
+//         geometry: 'polygon',
+//         basemap: 'voyager',
+//         extent: [-3.888962, 40.312064, -3.518051, 40.643271],
+//         dataset: 'barrios',
+//         column: 'nomdis',
+//         columnType: 'string',
+//         colorRamp: 'Bold'
+//       }
+//     }
+//   }
+// };
 
-// Create Vanilla scaffolding
-const fileDefinitions = Scaffoldings.vanilla(convertToTemplate(baseData));
-// generateBundle(fileDefinitions.files);
-console.log(fileDefinitions)
+
+export default function (baseData) {
+  // Create Vanilla scaffolding
+  return Scaffoldings.vanilla(convertToTemplate(baseData));
+  // generateBundle(fileDefinitions.files);
+}
