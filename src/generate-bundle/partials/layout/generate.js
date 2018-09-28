4ff86442dd32dfb844d86871a56665d0f20f756c
jesusbotella <jesus.botella@gmail.com>
2018-09-28 13:42:51 +0200

    Do not render toolbar if not present. Fix cartojs bugs

--- src/generate-bundle/partials/layout/generate.js
+++ src/generate-bundle/partials/layout/generate.js
@@ -9,7 +9,7 @@ import generateMapFooter from './map-footer/generate';
 import layout from './layout.template.html';
 
 export default function generateLayout (layoutData) {
-  const toolbar = layoutData.toolbar ? generateToolbar(layoutData.toolbar) : '';
+  const toolbar = layoutData.toolbar && layoutData.toolbar.position ? generateToolbar(layoutData.toolbar) : '';
   const sidebars = layoutData.sidebars && layoutData.sidebars.length ? layoutData.sidebars.map(generateSidebar) : [];
   const map = generateMap(layoutData.map);
   const panels = layoutData.panels && layoutData.panels.length ? generatePanels(layoutData.panels) : [];
